7f106cee966083df758d6b56db1aee2e174552ff
elbertwang3 <elbert_wang@alumni.brown.edu>
2018-07-21 17:12:10 -0700

    about me

--- src/js/components/About.js
+++ src/js/components/About.js
@@ -7,11 +7,14 @@ class About extends Component {
 
   render() {
     return (
-      <div className="aboutme">Hi, I'm Elbert. I recently graduated from Brown University,
-        where I studied Computer Science and Classics. I'm interested in data journalism, visualization, and storytelling.
-       I am currently an intern on the interactives team the SF Chronicle building charts and tools, and I used to be a data 
-       journalist at Dremio, where I use the company's product to analyze datasets write visual stories.  
-       Feel free to contact me at elbertwang1@gmail.com. </div>
+      <div className="aboutme">Hi, I'm Elbert. I am a recent graduate of Brown University,
+        where I studied Computer Science and Classics. At Brown, I learned how to code and to write, giving me the tools
+        to tell stories with code and fostering in me an interest in data journalism, visualization, and storytelling.
+       I am currently an intern on the interactives team at the San Francisco Chronicle building charts and tools. I will be a
+       fellow on the Data Visuals fellow at the Texas Tribune in the fall. I used to work at 
+       <a href="https://www.dremio.com/" target="_blank">Dremio</a> as a data journalist, where I used the company's technology
+        to analyze datasets and write visual stories. I am currently looking for full-time opportunities for 2019.
+       Feel free to contact me at elbertwang1@gmail.com or check out my work. </div>
     );
   }
 }
